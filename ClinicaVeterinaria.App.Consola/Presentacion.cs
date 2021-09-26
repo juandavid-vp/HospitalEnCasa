879d2c7aafb4b07c806e8161f293c97b557a76f4
MrA1610 <90330863+MrA1610@users.noreply.github.com>
2021-09-25 19:54:05 -0500

    Dominio clases

---
+++ ClinicaVeterinaria.App.Consola/Presentacion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ClinicaVeterinaria.App.Dominio;
+using ClinicaVeterinaria.App.Persistencia;
+
+namespace ClinicaVeterinaria.App.Consola
+{
+    class Presentacion
+    {
+        private static IRepositorioOwner _repoOwner = new RepositorioOwner (new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Usuario Final, aqui podras interactuar con nuestro Software!");
+            AddOwner();
+        }
+        private static void AddOwner()
+        {
+            var owner = new Owner
+            {
+                Nombre = "Persona nueva",
+                Ciudad = "La imaginacion",
+                Direccion = "Av creacion VIVA R",
+                CorreoElectronico = "Programando@live.com",
+                FechaNacimiento = new DateTime(992, 02, 12),
+                NumeroTelefono = "3222333223"
+            };
+            _repoOwner.AddOwner(owner);
+        }
+        private static void BuscarOwner(int idOwner)
+        {
+            var dueño = _repoOwner.GetOwner(idOwner);
+            Console.WriteLine(dueño.Nombre);
+        }
---
+++ ClinicaVeterinaria.App.Dominio/Entidades/Perro.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinaria.App.Dominio.Entidades
+{
+    public class Perro : Mascota
+    {
+        
+    }
+}
