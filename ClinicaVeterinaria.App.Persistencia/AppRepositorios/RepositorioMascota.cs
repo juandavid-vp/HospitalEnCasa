2e86180fbc79564930be96ba0208faa11000454b
Yaqueline Samboni Diaz <89922241+Yaquesam19@users.noreply.github.com>
2021-10-12 12:25:56 -0500

    Update RepositorioMascota.cs

--- ClinicaVeterinaria.App.Persistencia/AppRepositorios/RepositorioMascota.cs
+++ ClinicaVeterinaria.App.Persistencia/AppRepositorios/RepositorioMascota.cs
@@ -8,13 +8,18 @@ namespace ClinicaVeterinaria.App.Persistencia
     public class RepositorioMascota : IRepositorioMascota
     {
         private readonly AppContext _appContext;
+        private readonly Security security ;
         public RepositorioMascota(AppContext appContext)
         {
             this._appContext = appContext;
+            this.security = new Security();
         }
 
         public Mascota addMascotas(Mascota mascota)
         {
+            String password = mascotas.Password;
+            password = security.GetMD5Hash(password);
+            mascota.password = password;
             Mascota newMascota = _appContext.Add(mascota).Entity;
             _appContext.SaveChanges();
             return newMascota;
@@ -26,6 +31,7 @@ namespace ClinicaVeterinaria.App.Persistencia
             Mascota mascotaEncontrado = _appContext.Mascotas.FirstOrDefault(m => m.MascotaId == mascota.MascotaId);
             if (mascotaEncontrado != null)
             {
+                Mascota mascota
                 mascotaEncontrado.MascotaId = mascota.MascotaId;
                 mascotaEncontrado.NombreM = mascota.NombreM;
                 mascotaEncontrado.Nacimiento = mascota.Nacimiento;
